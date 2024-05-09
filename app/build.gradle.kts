49fdf1f905779d66c8bac055c16b972e583362dc
Tomasz Łabiński <tomeklab@gmail.com>
2024-05-09 17:30:09 +0200

    Adding dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,19 @@ android {
 
 dependencies {
 
+    // Adding my dependencies
+    // Compose ViewModel
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
+    // Network calls
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    // Json to Kotlin object mapping
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    // Navigation
+    implementation("androidx.navigation:navigation-compose:2.7.7")
+    //Image loading
+    implementation("io.coil-kt:coil-compose:2.4.0")
+
+
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.9.0")
